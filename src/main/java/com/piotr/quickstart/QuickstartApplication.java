2db7843284b513345421ee7d9a2dbbdb894c7032
Piotr Owczarczyk <piotr@Piotrs-MBP.fritz.box>
2024-02-23 18:46:30 +0100

    initial commit

---
+++ src/main/java/com/piotr/quickstart/QuickstartApplication.java
@@ -0,0 +1,13 @@
+package com.piotr.quickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // entry point to our Spring Boot app
+public class QuickstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickstartApplication.class, args);
+	}
+
+}
